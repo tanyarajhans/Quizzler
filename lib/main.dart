f4394f8c588f1b163829d889962bad3212ffb838
Tanya <tanyarajhans7@gmail.com>
2021-12-14 14:21:37 +0530

    Added abstraction by using quiz brain

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'question.dart';
+import 'quiz_brain.dart';
+
+QuizBrain quizBrain = QuizBrain();
 
 void main() => runApp(Quizzler());
 
@@ -39,11 +42,7 @@ class _QuizPageState extends State<QuizPage> {
 
   int questionNumber=0;
 
-  List<Question> questionBank = [
-    Question(q:'You can lead a cow down stairs but not up stairs.', a:false),
-    Question(q:'Approximately one quarter of human bones are in the feet.', a:true),
-    Question(q:'A slug\'s blood is green.',a:true)
-  ];
+  
 
   // List<bool> answers = [
   //   false, true, true
@@ -63,7 +62,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                questionBank[questionNumber].questionText,
+                quizBrain.questionBank[questionNumber].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -88,7 +87,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
-                bool correctAnswer=questionBank[questionNumber].questionAnswer;
+                bool correctAnswer=quizBrain.questionBank[questionNumber].questionAnswer;
                 if(correctAnswer==true){
                   setState((){
                   scorekeeper.add(
@@ -129,7 +128,7 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
-                bool correctAnswer=questionBank[questionNumber].questionAnswer;
+                bool correctAnswer=quizBrain.questionBank[questionNumber].questionAnswer;
                 if(correctAnswer=false){
                   setState((){
                   scorekeeper.add(
